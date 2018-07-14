d2af0d26e5ee17ff05bb9228197eeabcff87926e
deepankar.bhowmick <deepankar.bhowmick@accenture.com>
2018-07-14 16:19:02 +0530

    Added Comments.

--- Calculator/src/Calulator.groovy
+++ Calculator/src/Calulator.groovy
@@ -3,7 +3,7 @@ class Calulator {
 
 	public static void main(String[] args) 
 	{
-		Integer a, b, c
+		Integer a, b, c //Define the variables.
 		a = args[0].toInteger()
 		b = args[1].toInteger()
 		c = a + b
