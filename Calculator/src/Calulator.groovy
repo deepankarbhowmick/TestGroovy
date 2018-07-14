e59b0c3057d20335c3723a5e196c1912770c6d72
deepankar.bhowmick <deepankar.bhowmick@accenture.com>
2018-07-14 20:56:08 +0530

    Changed the comment.

--- Calculator/src/Calulator.groovy
+++ Calculator/src/Calulator.groovy
@@ -3,7 +3,7 @@ class Calulator {
 
 	public static void main(String[] args) 
 	{
-		//Add two numbers.
+		//Add two numbers and print the sum.
 		Integer a, b, c //Define the variables.
 		a = args[0].toInteger() //Get the first number.
 		b = args[1].toInteger() //Get the second number.
