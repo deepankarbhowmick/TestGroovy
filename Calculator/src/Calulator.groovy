a6954acafb34af8c3c8e9cffb86ee42a6b150f07
deepankar.bhowmick <deepankar.bhowmick@accenture.com>
2018-07-14 16:40:53 +0530

    Added more comments + changed the TEST.txt file.

--- Calculator/src/Calulator.groovy
+++ Calculator/src/Calulator.groovy
@@ -4,10 +4,10 @@ class Calulator {
 	public static void main(String[] args) 
 	{
 		Integer a, b, c //Define the variables.
-		a = args[0].toInteger()
-		b = args[1].toInteger()
-		c = a + b
-		println c
+		a = args[0].toInteger() //Get the first number.
+		b = args[1].toInteger() //Get the second number.
+		c = a + b //Add 2 numbers.
+		println c //Show output.
 	}
 
 }
