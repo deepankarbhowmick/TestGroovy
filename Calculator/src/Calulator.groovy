e3c2037aba7d28aaeda813dd59caf9792d6d0b07
deepankar.bhowmick <deepankar.bhowmick@accenture.com>
2018-07-14 17:27:21 +0530

    Stashed changes.

--- Calculator/src/Calulator.groovy
+++ Calculator/src/Calulator.groovy
@@ -3,11 +3,12 @@ class Calulator {
 
 	public static void main(String[] args) 
 	{
+		//Add two numbers.
 		Integer a, b, c //Define the variables.
 		a = args[0].toInteger() //Get the first number.
 		b = args[1].toInteger() //Get the second number.
 		c = a + b //Add 2 numbers.
-		println c //Show output.
+		println "The Sum of two numbers =" + c //Show output.
 	}
 
 }
