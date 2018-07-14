b11f099d8ad78af83734c7009f797b0dd67a28ce
deepankar.bhowmick <deepankar.bhowmick@accenture.com>
2018-07-14 20:58:08 +0530

    Branch feature changes.

--- Calculator/src/Calulator.groovy
+++ Calculator/src/Calulator.groovy
@@ -3,12 +3,12 @@ class Calulator {
 
 	public static void main(String[] args) 
 	{
-		//Add two numbers and print the sum.
+		//Substract two numbers and print the difference.
 		Integer a, b, c //Define the variables.
 		a = args[0].toInteger() //Get the first number.
 		b = args[1].toInteger() //Get the second number.
-		c = a + b //Add 2 numbers.
-		println "The Sum of two numbers =" + c //Show output.
+		c = a - b //Add 2 numbers.
+		println "The difference of two numbers =" + c //Show output.
 	}
 
 }
